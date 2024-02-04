759eba5021228a3cede9ecbdb6e4f08c83a80d00
Matheus Novaes <matheusfalcao.novaes@gmail.com>
2024-02-04 16:08:55 -0300

    update docker-compose file to run projects

--- data/influx/populate_influxdb.sh
+++ data/influx/populate_influxdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-influx restore --token "${INFLUX_ADMIN_TOKEN}" --host "${INFLUX_HOST}" --full populate_data/
+influx restore --token "${INFLUX_TOKEN}" --host "${INFLUX_HOST}" --full populate_data/
